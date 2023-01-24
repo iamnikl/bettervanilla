bbda3b1de1231791a6923fee2ff8e4087ca35a62
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-01-24 18:38:30 +0100

    added source code link to the footer

--- bettervanilla/src/routes/Footer.svelte
+++ bettervanilla/src/routes/Footer.svelte
@@ -1,4 +1,4 @@
 <hr>
-<footer class="container-fluid">
-    <small>Built with <a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico</a> and <a href="https://kit.svelte.dev/" target="_blank" rel="noopener noreferrer">SvelteKit</a></small>
+<footer class="container-fluid" style="text-align: right;">
+    <small>Built with <a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico</a> and <a href="https://kit.svelte.dev/" target="_blank" rel="noopener noreferrer">SvelteKit</a> <strong>·</strong> <a href="https://github.com/HACCKKER/bettervanilla" target="_blank" rel="noopener noreferrer">Source code</a></small>
 </footer>
