5c20b336bd7715c5dfdaab0e9a3fb0b37801adff
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-01-25 17:46:35 +0100

    theme switches back to selected when mouseleave

--- bettervanilla/src/routes/Header.svelte
+++ bettervanilla/src/routes/Header.svelte
@@ -2,33 +2,45 @@
     const storageKey = 'theme-preference'
     import { onMount } from "svelte"
     
-    onMount (async => {
+    onMount (() => {
+        setthemePreference();
+    });
+
+    // using click functions to close theme switcher for mobile devices compatibility
+    function themeswitcherCloseAuto() {
+        themeSwitcherClose();
+        setthemeAuto();
+        setPreference();
+    }
+
+    function themeswitcherCloseDark() {
+        themeSwitcherClose();
+        setthemeDark();
+        setPreference();
+    }
+
+    function themeswitcherCloseLight() {
+        themeSwitcherClose();
+        setthemeLight();
+        setPreference();
+    }
+
+    // main switcher functions
+    function themeSwitcherClose() {
+        document.getElementById('themeSelector').removeAttribute('open');
+    }
+
+    function setPreference() {
+        localStorage.setItem(storageKey, document.documentElement.getAttribute('data-theme'))
+    }
+    
+    function setthemePreference() {
         if (localStorage.getItem(storageKey))
             document.documentElement.setAttribute('data-theme', localStorage.getItem(storageKey));
         else
             setthemeAuto();
-    });
-
-    // redundancy for mobile devices compatibility
-    function themeswitcherCloseAuto() {
-        document.getElementById('themeSelector').removeAttribute('open');
-        setthemeAuto();
-        localStorage.setItem(storageKey, document.documentElement.getAttribute('data-theme'))
     }
 
-    function themeswitcherCloseDark() {
-        document.getElementById('themeSelector').removeAttribute('open');
-        setthemeDark();
-        localStorage.setItem(storageKey, document.documentElement.getAttribute('data-theme'))
-    }
-
-    function themeswitcherCloseLight() {
-        document.getElementById('themeSelector').removeAttribute('open');
-        setthemeLight();
-        localStorage.setItem(storageKey, document.documentElement.getAttribute('data-theme'))
-    }
-
-    // main switcher functions
     function setthemeAuto() {
         document.documentElement.setAttribute('data-theme', 'auto');
     }
@@ -51,9 +63,9 @@
             <details role="list" dir="rtl" id="themeSelector">
                 <summary aria-haspopup="listbox" role="link">Theme</summary>
                 <ul role="listbox">
-                    <li><a href={'#'} on:mouseover={setthemeAuto} on:focus={themeswitcherCloseAuto}>Auto</a></li>
-                    <li><a href={'#'} on:mouseover={setthemeDark} on:focus={themeswitcherCloseDark}>Dark</a></li>
-                    <li><a href={'#'} on:mouseover={setthemeLight} on:focus={themeswitcherCloseLight}>Light</a></li>
+                    <li><a href={'#'} on:mouseover={setthemeAuto} on:mouseleave={setthemePreference} on:focus={themeswitcherCloseAuto}>Auto</a></li>
+                    <li><a href={'#'} on:mouseover={setthemeDark} on:mouseleave={setthemePreference} on:focus={themeswitcherCloseDark}>Dark</a></li>
+                    <li><a href={'#'} on:mouseover={setthemeLight} on:mouseleave={setthemePreference} on:focus={themeswitcherCloseLight}>Light</a></li>
                 </ul>
             </details>
         </li>
