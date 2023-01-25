055557f0c2689c0a0242f3f0f5d4abb2a60bdd0a
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-01-25 16:17:02 +0100

    modified about page

--- bettervanilla/src/routes/about/+page.svelte
+++ bettervanilla/src/routes/about/+page.svelte
@@ -2,3 +2,8 @@
     <title>About</title>
     <meta name="description" content="About this website" />
 </svelte:head>
+
+<center>
+    <h1><kbd>WEBSITE IS UNDER CONSTRUCTION!!!</kbd></h1>
+    <small>Go back to the <a href="/">homepage</a></small>
+</center>
