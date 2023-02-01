17e29a51e2073b07bdbe3b7910888dbe7a351f8a
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-02-02 00:44:49 +0100

    added Sodium page

---
+++ bettervanilla/src/routes/docs/mods/Sodium/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Docs • Sodium</title>
+	<meta name="description" content="Why was Sodium added to the modpack" />
+</svelte:head>
+
+<hgroup>
+	<h1><a href="https://modrinth.com/mod/sodium" target="_blank" rel="noopener noreferrer">Sodium</a></h1>
+	<h2>An optimization mod</h2>
+</hgroup>
+<h3>Features</h3>
+<p>
+	This mod gives you access to advanced settings and greatly improves performance
+</p>
