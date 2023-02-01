858479375bc3604482396ec0610f975b4f362653
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-02-02 00:40:27 +0100

    removed global footer

--- bettervanilla/src/routes/Footer.svelte
+++
@@ -1,4 +0,0 @@
-<hr>
-<footer class="container-fluid" style="text-align: right;">
-    <small>Built with <a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico</a> and <a href="https://kit.svelte.dev/" target="_blank" rel="noopener noreferrer">SvelteKit</a> <strong>·</strong> <a href="https://github.com/HACCKKER/bettervanilla" target="_blank" rel="noopener noreferrer">Source code</a> <strong>·</strong> <a href="/about">About</a></small>
-</footer>
