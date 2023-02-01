a03647939daedd2fc47e15a3ad894de1a76d6e94
HACKKER <49511278+HACCKKER@users.noreply.github.com>
2023-02-01 20:58:05 +0100

    added docs page files

---
+++ bettervanilla/src/routes/docs/+layout.svelte
@@ -0,0 +1,25 @@
+<main class="container" style="padding-top: 4rem;">
+    <aside style="overflow-y: auto; overflow-x: hidden;">
+        <nav>
+            <details open="true">
+                <summary>Getting started</summary>
+                <ul>
+                    <li>
+                        <a class="secondary" href="/docs">Installation</a>
+                    </li>
+                </ul>
+            </details>
+            <details>
+                <summary>Mods</summary>
+                <ul>
+                    <li>
+                        <a class="secondary" href="/docs">Sodium</a>
+                    </li>
+                </ul>
+            </details>
+        </nav>
+    </aside>
+    <div role="document">
+        <slot />
+    </div>
+</main>
--- bettervanilla/src/routes/docs/+page.svelte
+++ bettervanilla/src/routes/docs/+page.svelte
@@ -2,3 +2,11 @@
     <title>Docs</title>
     <meta name="description" content="This page explains why each mod was included to the modpack" />
 </svelte:head>
+
+<section id="start">
+    <hgroup>
+        <h1>Installation</h1>
+        <h2>Sometext</h2>
+    </hgroup>
+    <p>Sometext</p>
+</section>
