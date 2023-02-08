3a51aa82bdd3b5e145ea7c7f81823b0f58176ed0
iamnikl <115219068+iamnikl@users.noreply.github.com>
2023-02-08 20:49:33 +0000

    modified:   bettervanilla/src/routes/about/+page.svelte 	new file:   bettervanilla/src/routes/impressum/+impressum.svelte 	new file:   bettervanilla/src/routes/sources/+page.ts 	new file:   bettervanilla/src/routes/sources/page.svelte 	new file:   bettervanilla/static/css/oldPico.css 	modified:   bettervanilla/static/css/pico/pico.css 	new file:   bettervanilla/static/css/scss/about/about.css 	new file:   bettervanilla/static/css/scss/about/about.css.map 	new file:   bettervanilla/static/css/scss/about/about.scss 	new file:   bettervanilla/static/css/scss/animations/mainAnimation.css 	new file:   bettervanilla/static/css/scss/animations/mainAnimation.css.map 	new file:   bettervanilla/static/css/scss/animations/mainAnimation.scss 	new file:   bettervanilla/static/css/scss/impressum/impressum.css 	new file:   bettervanilla/static/css/scss/main.css 	new file:   bettervanilla/static/css/scss/main.css.map 	new file:   bettervanilla/static/css/scss/main.scss 	new file:   bettervanilla/static/css/scss/start/start.css 	new file:   bettervanilla/static/css/scss/start/start.css.map 	new file:   bettervanilla/static/css/scss/start/start.scss 	new file:   bettervanilla/static/css/scss/tableConfig/tableFile.css 	new file:   bettervanilla/static/css/scss/tableConfig/tableFile.css.map 	new file:   bettervanilla/static/css/scss/tableConfig/tableFile.scss

--- bettervanilla/src/routes/about/+page.svelte
+++ bettervanilla/src/routes/about/+page.svelte
@@ -3,9 +3,83 @@
     <meta name="description" content="About this website" />
 </svelte:head>
 
-<div style="padding-top: 4rem;">
+
 <center>
-    <h1><kbd>WEBSITE IS UNDER CONSTRUCTION!!!</kbd></h1>
-    <small>Go back to the <a href="/">homepage</a></small>
+    <hgroup class="marginToHeader">
+        <h1>About</h1>
+        <h2>Informationen über unsere Website</h2>
+    </hgroup>
+    <p>Unsere Gruppe beschäftigt sich mit dem Minecraft Modpack <code>BetterVanilla</code></p>
+    <h4>Diese Website wurde erstellt von</h4>
+    <section id="developersContainer">
+        <a href="#a">
+            <article>
+                <h2>+</h2>
+                <h3>+</h3>
+            </article>
+        </a>
+        <a href="#leonie">
+            <article>
+                <h2>Leonie</h2>
+                <h3>Ø.</h3>
+            </article>
+        </a>
+        <a href="#niklas">
+            <article>
+                <h2>Niklas</h2>
+                <h3>P.</h3>
+            </article>
+        </a>
+    </section>
+    <!-- <small>Go back to the <a href="/">homepage</a></small> -->
 </center>
+<div id="personWorkInformations">
+    <article id="a">
+        <h3>+</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, dolorum? Sapiente natus accusamus consequuntur eos quibusdam facere, ducimus ullam tempora atque nobis cumque nam est voluptas doloremque laudantium veniam odit.</p>
+        <details>
+            <summary>
+                <h4 class="header-gray-light">Seine Arbeit</h4>
+            </summary>
+            <ul>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+            </ul>
+        </details>
+    </article>
+    <article id="leonie">
+        <h3>Leonie</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, dolorum? Sapiente natus accusamus consequuntur eos quibusdam facere, ducimus ullam tempora atque nobis cumque nam est voluptas doloremque laudantium veniam odit.</p>
+        <details>
+            <summary>
+                <h4 class="header-gray-light">Ihre Arbeit</h4>
+            </summary>
+            <ul>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+            </ul>
+        </details>
+    </article>
+    <article id="niklas">
+        <h3>Niklas</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, dolorum? Sapiente natus accusamus consequuntur eos quibusdam facere, ducimus ullam tempora atque nobis cumque nam est voluptas doloremque laudantium veniam odit.</p>
+        <details>
+            <summary>
+                <h4 class="header-gray-light">Seine Arbeit</h4>
+            </summary>
+            <ul>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+                <li>Header</li>
+            </ul>
+        </details>
+    </article>
 </div>
---
+++ bettervanilla/src/routes/sources/page.svelte
@@ -0,0 +1,89 @@
+<svelte:head>
+    <title>Sources</title>
+    <meta name="description" content="Modpack sources-list page" />
+</svelte:head>
+<!-- <head>
+    <link rel="stylesheet" href="../../../static/css/scss/main.css">
+    <link rel="stylesheet" href="../../../static/css/global.css">
+    
+    <link rel="stylesheet" href="../../../static/css/theme-green.css">
+    <link rel="stylesheet" href="../../../static/css/pico/pico.css">
+</head> -->
+
+<br/>
+<br/>
+<br/>
+
+<center>
+    <hgroup class="slideInAnimation">
+        <h1>{headerContent}</h1>
+        <h2>{sencondlyHeaderContent}</h2>
+    </hgroup>
+    
+    <table role="grid" class="referenceTable">
+        <thead>
+            <th>Description</th>
+            <th>Reference</th>
+        </thead>
+        <tbody>
+            <!-- Development -->
+            <tr>
+                <td colspan="2"><span data-undercategory="All">Development</span></td>
+            </tr>
+            <tr>
+                <td>Pico CSS</td>
+                <td>
+                    <a href="https://picocss.com/">&nbsp;</a>
+                </td>
+            </tr>
+            <tr>
+                <td>SASS / SCSS</td>
+                <td>
+                    <a href="https://sass-lang.com/">&nbsp;</a>
+                </td>
+            </tr>
+            <tr>
+                <td>SvelteKit</td>
+                <td>
+                    <a href="https://kit.svelte.dev/">&nbsp;</a>
+                </td>
+            </tr>
+            <tr>
+                <td>NodeJS</td>
+                <td>
+                    <a href="https://nodejs.org/">&nbsp;</a>
+                </td>
+            </tr>
+            <!-- Images -->
+            <tr>
+                <td colspan="2"><span data-undercategory="Home">Images</span></td>
+            </tr>
+            <tr>
+                <td>Sodium 0.4</td>
+                <td>
+                    <a href="https://modrinth.com/mod/sodium">&nbsp;</a>
+                </td>
+            </tr>
+            <tr>
+                <td>BetterCommandBlockUi</td>
+                <td>
+                    <a href="https://modrinth.com/mod/bettercommandblockui">&nbsp;</a>
+                </td>
+            </tr>
+            <tr>
+                <td>First Person Model</td>
+                <td>
+                    <a href="https://modrinth.com/mod/first-person-model">&nbsp;</a>
+                </td>
+            </tr>
+            <!-- ... -->
+            
+        </tbody>
+    </table>
+</center>
+
+<script lang="js">
+    const headerContent = "Externe Quellen";
+    const sencondlyHeaderContent = "Hier befinden sich die Links zu allen externen Quellen ";
+    
+</script>
